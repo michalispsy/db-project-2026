a4d5a271aa98da88000d0103ebf0619ed2988f18
Loukas <louk.frag@gmail.com>
2026-05-04 10:55:48 +0300

    install.sql first draft

--- semester_exe/sql/install.sql
+++ semester_exe/sql/install.sql
@@ -3,63 +3,272 @@ DROP DATABASE IF EXISTS ygeiopolis;
 CREATE DATABASE ygeiopolis;
 USE ygeiopolis;
 
-CREATE TABLE Departments (
-    dept_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+CREATE TABLE departments (
+    dept_id INT AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
     description TEXT,
-    beds_count INT DEFAULT 0,
-    floor_building VARCHAR(50)
-) ENGINE=InnoDB;
+    bed_count INT NOT NULL DEFAULT 0,
+    floor INT,
+    building VARCHAR(50),
+    director_AMKA CHAR(11), -- MAYBE ADD NULL OPTION
 
-CREATE TABLE Personnel (
-    AMKA VARCHAR(20) PRIMARY KEY,
+    CONSTRAINT pk_dept PRIMARY KEY (dept_id)
+);
+
+CREATE TABLE beds (
+    bed_id INT AUTO_INCREMENT,
+    dept_id INT NOT NULL,
+    room_capacity INT,
+    bed_type ENUM('regular', 'icu', 'emergency', 'pediatric') NOT NULL DEFAULT 'regular',
+    status ENUM('free', 'occupied', 'unavailable') NOT NULL DEFAULT 'free',
+    assigned_to INT DEFAULT NULL,
+    
+    CONSTRAINT pk_beds PRIMARY KEY (bed_id),
+    CONSTRAINT fk_beds_dept FOREIGN KEY (dept_id) REFERENCES departments(dept_id) ON DELETE CASCADE
+);
+
+CREATE TABLE staff (
+    AMKA char(11),
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    age INT,
-    email VARCHAR(100),
-    phone VARCHAR(20),
-    hire_date DATE,
-    staff_type ENUM('Doctor', 'Nurse', 'Administrative') NOT NULL
-) ENGINE=InnoDB;
+    date_of_birth DATE NOT NULL,
+    age INT AS (TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE())) VIRTUAL,
+    email VARCHAR(50) UNIQUE,
+    phone_number VARCHAR(15) UNIQUE,
+    hire_date DATE DEFAULT (CURRENT_DATE),
+    staff_type ENUM('doctor', 'admin', 'nurse'),
 
-CREATE TABLE Doctors (
-    AMKA VARCHAR(20) PRIMARY KEY,
-    license_number VARCHAR(50) UNIQUE,
-    specialty VARCHAR(100),
-    `rank` ENUM('Resident', 'Attending B', 'Attending A', 'Director'),
-    supervisor_amka VARCHAR(20),
-    FOREIGN KEY (AMKA) REFERENCES Personnel(AMKA) ON DELETE CASCADE,
-    FOREIGN KEY (supervisor_amka) REFERENCES Doctors(AMKA)
-) ENGINE=InnoDB;
+    CONSTRAINT pk_staff PRIMARY KEY (AMKA)
+);
 
-CREATE TABLE Patients (
-    AMKA VARCHAR(11) PRIMARY KEY,
+CREATE TABLE doctors (
+    AMKA CHAR(11),
+    license_number VARCHAR(20) NOT NULL,
+    rank ENUM('resident', 'junior attending', 'senior attending', 'director') NOT NULL,
+    supervisor_AMKA CHAR(11),
+
+    CONSTRAINT pk_doctors PRIMARY KEY (AMKA),
+    CONSTRAINT fk_doctors FOREIGN KEY (AMKA) REFERENCES staff(AMKA) ON DELETE CASCADE,
+    CONSTRAINT fk_docs_supervisor FOREIGN KEY (supervisor_AMKA) REFERENCES doctors(AMKA) ON DELETE SET NULL,
+    -- CONSTRAINT chk_resident_supervisor CHECK (CASE WHEN `rank` = 'resident' THEN supervisor_AMKA IS NOT NULL ELSE TRUE END),
+    -- CONSTRAINT chk_director_supervisor CHECK (CASE WHEN `rank` = 'director' THEN supervisor_AMKA IS NULL ELSE TRUE END),
+    CONSTRAINT uni_license UNIQUE (license_number)
+);
+
+CREATE TABLE doctor_departments (
+    doctor_AMKA CHAR(11),
+    dept_id INT NOT NULL,
+
+    CONSTRAINT pk_dd PRIMARY KEY (doctor_AMKA, dept_id),
+    CONSTRAINT fk_dd_amka FOREIGN KEY (doctor_AMKA) REFERENCES doctors(AMKA) ON DELETE CASCADE,
+    CONSTRAINT fd_dd_dept FOREIGN KEY (dept_id) REFERENCES departments(dept_id) ON DELETE CASCADE
+);
+
+CREATE TABLE admin_staff (
+    AMKA CHAR(11),
+    position VARCHAR(50) NOT NULL,
+    office_location VARCHAR(50) NOT NULL,
+    dept_id INT, -- MAYBE ADD NOT NULL
+    
+    CONSTRAINT pk_admin_staff PRIMARY KEY (AMKA),
+    CONSTRAINT fk_admin_staff FOREIGN KEY (AMKA) REFERENCES staff(AMKA) ON DELETE CASCADE,
+    CONSTRAINT fk_admin_dept FOREIGN KEY (dept_id) REFERENCES departments(dept_id) ON DELETE SET NULL -- MAYBE ADD RESTRICT
+);
+
+CREATE TABLE nurses (
+    AMKA CHAR(11),
+    rank ENUM('nursing assistant', 'registered nurse', 'head nurse') NOT NULL,
+    dept_id INT, -- MAYBE ADD NOT NULL
+
+    CONSTRAINT pk_nurses PRIMARY KEY (AMKA),
+    CONSTRAINT fk_nurses FOREIGN KEY (AMKA) REFERENCES staff(AMKA) ON DELETE CASCADE,
+    CONSTRAINT fk_nurses_dept FOREIGN KEY (dept_id) REFERENCES departments(dept_id) ON DELETE CASCADE -- MAYBE ADD RESTRICT
+);
+
+CREATE TABLE patients (
+    AMKA CHAR(11),
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    age INT,
-    gender ENUM('M', 'F', 'Other'),
-    insurance_provider VARCHAR(100)
-) ENGINE=InnoDB;
+    fathers_name VARCHAR(50),
+    date_of_birth DATE,
+    age INT AS (TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE())) VIRTUAL,
+    gender ENUM('M', 'F', 'Other') NOT NULL,
+    weight INT,
+    height INT,
+    phone_number VARCHAR(15) UNIQUE,
+    email VARCHAR(50) UNIQUE,
+    occupation VARCHAR(50),
+    nationality CHAR(2),
+    insurance_provider VARCHAR(50),
+    
+    CONSTRAINT pk_patients PRIMARY KEY (AMKA)
+);
 
-CREATE TABLE Beds (
-    bed_id INT AUTO_INCREMENT PRIMARY KEY,
-    dept_id INT,
-    bed_number VARCHAR(10) NOT NULL,
-    type ENUM('ICU', 'Single', 'Shared'),
-    status ENUM('Available', 'Occupied', 'Maintenance'),
-    FOREIGN KEY (dept_id) REFERENCES Departments(dept_id)
-) ENGINE=InnoDB;
+CREATE TABLE patient_emergency_contacts (
+    patient_AMKA CHAR(11),
+    contact_seq INT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    email VARCHAR(50),
+    relationship_to_patient VARCHAR(50),
 
-CREATE TABLE Hospitalizations (
-    hosp_id INT AUTO_INCREMENT PRIMARY KEY,
-    patient_amka VARCHAR(11),
-    bed_id INT,
-    dept_id INT,
-    entry_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    exit_date DATETIME,
-    diagnosis_entry TEXT,
-    total_cost DECIMAL(10, 2),
-    FOREIGN KEY (patient_amka) REFERENCES Patients(AMKA),
-    FOREIGN KEY (bed_id) REFERENCES Beds(bed_id),
-    FOREIGN KEY (dept_id) REFERENCES Departments(dept_id)
-) ENGINE=InnoDB;
+    CONSTRAINT pk_ec PRIMARY KEY (contact_seq, patient_AMKA),
+    CONSTRAINT fk_ec_patient FOREIGN KEY (patient_AMKA) REFERENCES patients(AMKA) ON DELETE CASCADE
+);
+
+CREATE TABLE ken (
+    ken_code VARCHAR(5),
+    description TEXT NOT NULL,
+    base_cost DECIMAL(10, 2) NOT NULL,
+    avg_length_of_stay INT,
+    daily_surcharge_rate DECIMAL(10, 2),
+
+    CONSTRAINT pk_ken PRIMARY KEY (ken_code)
+);
+
+CREATE TABLE icd10 (
+    icd10_code VARCHAR(6),
+    description TEXT NOT NULL,
+    category VARCHAR(50),
+    
+    CONSTRAINT pk_icd10 PRIMARY KEY (icd10_code)
+);
+
+CREATE TABLE admissions (
+    admission_id INT AUTO_INCREMENT,
+    patient_AMKA CHAR(11) NOT NULL,
+    department_id INT NOT NULL,
+    bed_id INT NOT NULL UNIQUE,
+    ken_code VARCHAR(5) NOT NULL,
+    admission_date DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    discharge_date DATE DEFAULT NULL,
+    admission_diagnosis_code VARCHAR(6) NOT NULL,
+    discharge_diagnosis_code VARCHAR(6) DEFAULT NULL,
+    base_cost  DECIMAL(10, 2) DEFAULT 0.00,
+    extra_cost DECIMAL(10, 2) DEFAULT 0.00,
+    total_cost DECIMAL(10, 2) GENERATED ALWAYS AS (base_cost + extra_cost) VIRTUAL,
+
+    CONSTRAINT pk_admissions PRIMARY KEY (admission_id),
+    CONSTRAINT fk_adm_patient FOREIGN KEY (patient_AMKA) REFERENCES patients(AMKA) ON DELETE CASCADE,
+    CONSTRAINT fk_adm_dept FOREIGN KEY (department_id) REFERENCES departments(dept_id),
+    CONSTRAINT fk_adm_bed FOREIGN KEY (bed_id) REFERENCES beds(bed_id),
+    CONSTRAINT fk_adm_ken FOREIGN KEY (ken_code) REFERENCES ken(ken_code),
+    CONSTRAINT fk_adm_diag_in FOREIGN KEY (admission_diagnosis_code) REFERENCES icd10(icd10_code),
+    CONSTRAINT fk_adm_diag_out FOREIGN KEY (discharge_diagnosis_code) REFERENCES icd10(icd10_code)
+);
+
+CREATE TABLE triages (
+    triage_id INT AUTO_INCREMENT,
+    patient_AMKA CHAR(11) NOT NULL,
+    nurse_AMKA CHAR(11) NOT NULL,
+    arrival_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    triage_time DATETIME DEFAULT NULL,
+    minutes_waited INT DEFAULT NULL,
+    urgency_level INT CHECK (urgency_level BETWEEN 1 AND 5),
+    symptoms TEXT,
+    outcome ENUM('hospitalized', 'discharged') DEFAULT 'discharged',
+    admission_id INT DEFAULT NULL UNIQUE,
+
+    CONSTRAINT pk_triages PRIMARY KEY (triage_id),
+    CONSTRAINT fk_triage_patient FOREIGN KEY (patient_AMKA) REFERENCES patients(AMKA),
+    CONSTRAINT fk_triage_nurse FOREIGN KEY (nurse_AMKA) REFERENCES nurses(AMKA),
+    CONSTRAINT fk_triage_admission FOREIGN KEY (admission_id) REFERENCES admissions(admission_id)
+);
+
+CREATE TRIGGER beds_insert_trigger
+AFTER INSERT ON beds
+FOR EACH ROW
+UPDATE departments 
+SET bed_count = bed_count + 1 
+WHERE dept_id = NEW.dept_id;
+
+CREATE TRIGGER beds_delete_trigger
+AFTER DELETE ON beds
+FOR EACH ROW
+UPDATE departments 
+SET bed_count = bed_count - 1 
+WHERE dept_id = OLD.dept_id;
+
+CREATE TRIGGER set_contact_seq_trigger
+BEFORE INSERT ON patient_emergency_contacts
+FOR EACH ROW
+BEGIN
+    SET NEW.contact_seq = (
+        SELECT COALESCE(MAX(contact_seq), 0) + 1 
+        FROM patient_emergency_contacts 
+        WHERE patient_AMKA = NEW.patient_AMKA
+    );
+END;
+
+CREATE TRIGGER calculate_admission_base_cost_trigger
+BEFORE INSERT ON admissions
+FOR EACH ROW
+BEGIN
+    DECLARE v_cost DECIMAL(10, 2);
+    
+    SELECT base_cost
+    INTO v_cost
+    FROM ken
+    WHERE ken_code = NEW.ken_code;
+    
+    SET NEW.base_cost = v_cost, NEW.extra_cost = 0.00;
+END;
+
+CREATE TRIGGER calculate_admission_extra_cost_trigger
+BEFORE UPDATE ON admissions
+FOR EACH ROW
+BEGIN
+    DECLARE v_avg_length_of_stay INT;
+    DECLARE v_days_stayed INT;
+    DECLARE v_daily_surcharge_rate DECIMAL (10, 2);
+    DECLARE v_base_cost DECIMAL (10, 2);
+
+    IF OLD.ken_code <> NEW.ken_code THEN
+        SELECT base_cost
+        INTO v_base_cost
+        FROM ken WHERE
+        ken_code = NEW.ken_code;
+
+        SET NEW.base_cost = v_base_cost;
+
+    ELSEIF NEW.base_cost <> OLD.base_cost THEN
+        SET NEW.base_cost = OLD.base_cost;
+    END IF;
+
+    IF OLD.discharge_date IS NULL AND NEW.discharge_date IS NOT NULL THEN
+        SELECT avg_length_of_stay, daily_surcharge_rate 
+        INTO v_avg_length_of_stay, v_daily_surcharge_rate
+        FROM ken 
+        WHERE ken_code = NEW.ken_code;
+
+        SET v_days_stayed = DATEDIFF(NEW.discharge_date, NEW.admission_date);
+        IF v_days_stayed = 0 THEN SET v_days_stayed = 1; END IF;
+
+        IF v_days_stayed > v_avg_length_of_stay THEN
+            SET NEW.extra_cost = (v_days_stayed - v_avg_length_of_stay) * v_daily_surcharge_rate;
+        ELSE
+            SET NEW.extra_cost = 0.00;
+        END IF;
+    END IF;
+END;
+
+CREATE TRIGGER set_bed_occupied_trigger
+AFTER INSERT ON admissions
+FOR EACH ROW
+BEGIN
+    UPDATE beds 
+    SET status = 'occupied', assigned_to = NEW.admission_id
+    WHERE bed_id = NEW.bed_id;
+END;
+
+CREATE TRIGGER set_bed_free_trigger
+AFTER UPDATE ON admissions
+FOR EACH ROW
+BEGIN
+    IF OLD.discharge_date IS NULL AND NEW.discharge_date IS NOT NULL THEN
+        UPDATE beds 
+        SET status = 'free', assigned_to = NULL
+        WHERE bed_id = NEW.bed_id;
+    END IF;
+END;
