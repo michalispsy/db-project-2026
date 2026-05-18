081457a7eb4910adc4df18cc6d8afec397fc74fc
Loukas <louk.frag@gmail.com>
2026-05-18 18:17:51 +0300

    fffffffFF?

--- semester_exe/sql/Q13.sql
+++ semester_exe/sql/Q13.sql
@@ -5,7 +5,7 @@ WITH RECURSIVE supervisor_hierarchy AS (
         d.supervisor_amka AS next_supervisor_amka,
         CAST(CONCAT(s_leaf.first_name, ' ', s_leaf.last_name) AS CHAR(100)) AS doctor_name,
         CASE WHEN d.supervisor_amka IS NULL THEN 1 ELSE 2 END AS level,
-        CAST(CASE WHEN d.supervisor_amka IS NULL THEN '/' ELSE CONCAT('/', s_sup.first_name, ' ', s_sup.last_name, '/') END AS CHAR(500)) AS hierarchy_path
+        CAST(CASE WHEN d.supervisor_amka IS NULL THEN '/' ELSE CONCAT('/', s_sup.first_name, '-', s_sup.last_name, '/') END AS CHAR(500)) AS hierarchy_path
     FROM doctors d
     JOIN staff s_leaf ON d.amka = s_leaf.amka
     LEFT JOIN staff s_sup ON d.supervisor_amka = s_sup.amka
@@ -15,7 +15,7 @@ WITH RECURSIVE supervisor_hierarchy AS (
         d.supervisor_amka,
         sh.doctor_name,
         sh.level + 1,
-        CONCAT('/', s.first_name, ' ', s.last_name, sh.hierarchy_path)
+        CONCAT('/', s.first_name, '-', s.last_name, sh.hierarchy_path)
     FROM supervisor_hierarchy sh
     INNER JOIN doctors d ON sh.next_supervisor_amka = d.amka
     INNER JOIN staff s ON d.supervisor_amka = s.amka
@@ -23,8 +23,8 @@ WITH RECURSIVE supervisor_hierarchy AS (
 )
 SELECT
     doctor_name,
-    hierarchy_path,
-    level
+    level,
+    hierarchy_path
 FROM (
     SELECT *, ROW_NUMBER() OVER(PARTITION BY leaf_amka ORDER BY level DESC) as `number`
     FROM supervisor_hierarchy
