a13c3007045720fd6273cf32e810bc713582873e
michalis.psy <michalis.psychis@gmail.com>
2026-05-20 16:14:06 +0300

    sql changes

--- semester_exe/sql/Q04_a_explain.sql
+++ semester_exe/sql/Q04_a_explain.sql
@@ -4,6 +4,7 @@ DROP INDEX IF EXISTS idx_staff_last_name ON staff;
 
 EXPLAIN SELECT
     doc.AMKA,
+    s.first_name,
     s.last_name,
     ROUND(AVG(dr.medical_care_quality), 2) AS avg_medical_care_quality,
     ROUND(AVG(ar.overall), 2)              AS avg_overall_admission_rating,
@@ -13,5 +14,5 @@ JOIN staff s           ON doc.AMKA          = s.AMKA
 JOIN doctor_ratings dr ON doc.AMKA          = dr.doctor_AMKA
 JOIN admissions a      ON dr.admission_id   = a.admission_id
 JOIN admission_ratings ar ON a.admission_id = ar.admission_id
-WHERE s.last_name = 'Alexiou'
-GROUP BY doc.AMKA, s.last_name;
+WHERE s.last_name = 'Alexiou' AND s.first_name = 'Michalis'
+GROUP BY doc.AMKA, s.first_name, s.last_name;
--- semester_exe/sql/Q04_c_timing.sql
+++ semester_exe/sql/Q04_c_timing.sql
@@ -7,6 +7,7 @@ DROP INDEX IF EXISTS idx_staff_last_name ON staff;
 
 SELECT
     doc.AMKA,
+    s.first_name,
     s.last_name,
     ROUND(AVG(dr.medical_care_quality), 2) AS avg_medical_care_quality,
     ROUND(AVG(ar.overall), 2)              AS avg_overall_admission_rating,
@@ -16,14 +17,15 @@ JOIN staff s           ON doc.AMKA          = s.AMKA
 JOIN doctor_ratings dr ON doc.AMKA          = dr.doctor_AMKA
 JOIN admissions a      ON dr.admission_id   = a.admission_id
 JOIN admission_ratings ar ON a.admission_id = ar.admission_id
-WHERE s.last_name = 'Alexiou'
-GROUP BY doc.AMKA, s.last_name;
+WHERE s.last_name = 'Alexiou' AND s.first_name = 'Michalis'
+GROUP BY doc.AMKA, s.first_name, s.last_name;
 
 -- Έκδοση 2: με FORCE INDEX
 CREATE INDEX IF NOT EXISTS idx_staff_last_name ON staff(last_name);
 
 SELECT
     doc.AMKA                               AS doctor_AMKA,
+    s.first_name                           AS doctor_first_name,
     s.last_name                            AS doctor_last_name,
     ROUND(AVG(dr.medical_care_quality), 2) AS avg_medical_care_quality,
     ROUND(AVG(ar.overall), 2)              AS avg_overall_admission_rating,
@@ -33,8 +35,8 @@ JOIN staff s FORCE INDEX (idx_staff_last_name) ON doc.AMKA = s.AMKA
 JOIN doctor_ratings dr ON doc.AMKA          = dr.doctor_AMKA
 JOIN admissions a      ON dr.admission_id   = a.admission_id
 JOIN admission_ratings ar ON a.admission_id = ar.admission_id
-WHERE s.last_name = 'Alexiou'
-GROUP BY doc.AMKA, s.last_name;
+WHERE s.last_name = 'Alexiou' AND s.first_name = 'Michalis'
+GROUP BY doc.AMKA, s.first_name, s.last_name;
 
 -- Query_ID 1 = χωρίς index, Query_ID 2 = με FORCE INDEX
 SHOW PROFILES;
